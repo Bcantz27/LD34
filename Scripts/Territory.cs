e48887965f78fb36cb1faa897c5d8679bad0e79e
Bryan Cantz <bryancantz@comcast.net>
2015-12-12 19:27:12 -0500

    Level design

---
+++ Scripts/Territory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Territory : MonoBehaviour {
+
+    public int followers = 5;
+    public int influenceNeeded = 25;
+    public int bosses = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onTriggerEnter(Collider other){
+        Debug.Log("Enter");
+    }
+
+    void onTriggerExit(Collider other)
+    {
+        Debug.Log("Enter");
+    }
+}
